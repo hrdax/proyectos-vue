c3cd61e6c2877e8e27a481578ef79b22c80be501
hrdax <juanpa_200018@hotmail.com>
2023-08-18 18:18:27 -0400

    obteniendo tweets y verificando si es que hay

--- tweet-simulator/src/api/tweet.js
+++ tweet-simulator/src/api/tweet.js
@@ -2,12 +2,33 @@ import { v4 as uuidv4 } from 'uuid'
 import { TWEETS } from '../utils/constants'
 
 export function saveTweetApi(tweet, username) {
-    const tweetTemp = {
-        id: uuidv4(),
-        tweet,
-        username,
-        createdAt: new Date()
+    const tweets = getTweetApi()
+    
+    if(tweets.length === 0) {
+        const tweetTemp = [
+        {
+            id: uuidv4(),
+            tweet,
+            username,
+            createdAt: new Date()
+        }]
+        localStorage.setItem(TWEETS, JSON.stringify(tweetTemp))
+    } else {
+        tweets.push({
+            id: uuidv4(),
+            tweet,
+            username,
+            createdAt: new Date()
+        })
+        localStorage.setItem(TWEETS, JSON.stringify(tweets))
     }
-    console.log(tweetTemp)
-    localStorage.setItem(TWEETS, JSON.stringify(tweetTemp))
+    
+}
+
+export function getTweetApi() {
+    const tweets = localStorage.getItem(TWEETS)
+    if (tweets) {
+        return JSON.parse(tweets)
+    }
+    return []
 }
