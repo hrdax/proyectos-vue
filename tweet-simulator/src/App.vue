26e0b0f38b8cbbf32bd9de0d06aa08c6dabc385a
hrdax <juanpa_200018@hotmail.com>
2023-08-18 18:34:23 -0400

    importando componente tweetlist

--- tweet-simulator/src/App.vue
+++ tweet-simulator/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <Menu :openCloseForm="openCloseForm" :showForm="showForm"/>
   <TweetForm :showForm="showForm" />
+  <TweetList />
 </template>
 
 <script>
 import Menu from './components/Menu.vue'
 import TweetForm from './components/TweetForm.vue'
 import useFormTweet from './hooks/useFormTweet'
+import TweetList from './components/TweetList.vue'
 
 export default {
   name: 'App',
   components: {
     Menu,
     TweetForm,
+    TweetList,
   },
   setup() {
     return {
