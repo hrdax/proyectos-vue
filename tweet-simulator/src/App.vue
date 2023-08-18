6ae613e6093586d3d0fb8617ef010d7e82551deb
hrdax <juanpa_200018@hotmail.com>
2023-08-18 17:28:28 -0400

    componente tweetform y funciones del tweet

--- tweet-simulator/src/App.vue
+++ tweet-simulator/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <Menu />
+  <TweetForm />
 </template>
 
 <script>
 import Menu from './components/Menu.vue'
+import TweetForm from './components/TweetForm.vue'
 
 export default {
   name: 'App',
   components: {
     Menu,
+    TweetForm,
+
   }
   
 }
---
+++ tweet-simulator/src/components/TweetForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tweet-form container">
+    <form>
+        <input class="form-control" placeholder="Tu Nombre" />
+        <textarea class="form-control" rows="3" placeholder="Escribe tu tweet"></textarea>
+        <button class="btn btn-primary">
+            Enviar Tweet
+        </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.tweet-form {
+    margin-top: 20px;
+    // height: 0px;
+    overflow: hidden;
+
+    form {
+        margin-bottom: 50px;
+
+        input {
+            margin-bottom: 10px;
+        }
+
+        button {
+            width: 100%;
+            margin-top: 10px;
+        }
+    }
+}
+</style>
