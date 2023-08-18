e3ceb7e48adfd1bf5e7cddb95e45569009cc3f4a
hrdax <juanpa_200018@hotmail.com>
2023-08-18 17:46:51 -0400

    importando, uso de hooks

--- tweet-simulator/src/App.vue
+++ tweet-simulator/src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <Menu />
-  <TweetForm />
+  <Menu :openCloseForm="openCloseForm" :showForm="showForm"/>
+  <TweetForm :showForm="showForm" />
 </template>
 
 <script>
 import Menu from './components/Menu.vue'
 import TweetForm from './components/TweetForm.vue'
+import useFormTweet from './hooks/useFormTweet'
 
 export default {
   name: 'App',
   components: {
     Menu,
     TweetForm,
-
+  },
+  setup() {
+    return {
+      ...useFormTweet(),
+    }
   }
   
 }
