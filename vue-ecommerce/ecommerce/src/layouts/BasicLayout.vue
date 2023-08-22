863938166867fa980b9583c3d20d4d6422ad2ee9
hrdax <juanpa_200018@hotmail.com>
2023-08-22 17:02:29 -0400

    componente layout y probandolo

---
+++ vue-ecommerce/ecommerce/src/layouts/BasicLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="basic-layout">
+        <h2>MENU...</h2>
+
+        <div class="ui container">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BasicLayout',
+}
+</script>
+
+<style>
+
+</style>
--- vue-ecommerce/ecommerce/src/views/HomeView.vue
+++ vue-ecommerce/ecommerce/src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <button class="ui button">Follow</button>
+    <BasicLayout> probando layout </BasicLayout>
+    
   </div>
 </template>
 
 <script>
+import BasicLayout from '../layouts/BasicLayout'
 
 export default {
   name: 'Home',
   components: {
-    
+    BasicLayout,
+
   }
 }
 </script>
