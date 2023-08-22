3b17610013060cf9ab9f67af2907a8d29f00b385
hrdax <juanpa_200018@hotmail.com>
2023-08-22 17:33:41 -0400

    componentes login y register

---
+++ vue-ecommerce/ecommerce/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <BasicLayout>
+        <h1>Login</h1>
+
+        <router-link to="/register">
+            Crear Usuario
+        </router-link>
+    </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from '../layouts/BasicLayout'
+
+export default {
+    name: 'Login',
+    components: {
+        BasicLayout,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-ecommerce/ecommerce/src/views/Register.vue
@@ -0,0 +1,24 @@
+<template>
+    <BasicLayout>
+        <h1>Register</h1>
+
+        <router-link to="/login">
+            Iniciar Sesión
+        </router-link>
+    </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from '../layouts/BasicLayout'
+
+export default {
+    name: 'Register',
+    components: {
+        BasicLayout,
+    }
+}
+</script>
+
+<style>
+
+</style>
