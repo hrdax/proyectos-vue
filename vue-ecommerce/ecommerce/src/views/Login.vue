90ae761615306942adf87d4c3111ce56c34b3255
hrdax <juanpa_200018@hotmail.com>
2023-08-22 18:35:38 -0400

    formulario login

--- vue-ecommerce/ecommerce/src/views/Login.vue
+++ vue-ecommerce/ecommerce/src/views/Login.vue
@@ -1,10 +1,23 @@
 <template>
     <BasicLayout>
-        <h1>Login</h1>
+        <h1>Iniciar Sesión</h1>
+        <div class="login">
+            <form class="ui form">
+                <div class="field">
+                    <input type="text" placeholder="Nombre de usuario" />
+                </div>
+                <div class="field">
+                    <input type="password" placeholder="Contraseña" />
+                </div>
+                <button type="submit" class="ui button fluid primary">
+                    Iniciar Sesión
+                </button>
+            </form>
+            <router-link to="/register">
+                Crear Usuario
+            </router-link>
+        </div>
 
-        <router-link to="/register">
-            Crear Usuario
-        </router-link>
     </BasicLayout>
 </template>
 
