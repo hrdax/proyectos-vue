d130cabe2b3af40bc9160f462bdf2d80965230df
hrdax <juanpa_200018@hotmail.com>
2023-08-22 19:44:20 -0400

    getTokenApi

--- vue-ecommerce/ecommerce/src/api/token.js
+++ vue-ecommerce/ecommerce/src/api/token.js
@@ -3,3 +3,7 @@ import { TOKEN } from '../utils/constants'
 export function setTokenApi(token) {
     localStorage.setItem(TOKEN, token)
 }
+
+export function getTokenApi() {
+    return localStorage.getItem(TOKEN)
+}
