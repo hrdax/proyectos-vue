f39a36b730dd01c28fee77e5e89b003d6f6d2247
hrdax <juanpa_200018@hotmail.com>
2023-08-23 16:34:27 -0400

    componente producto

---
+++ vue-ecommerce/client/src/components/Product.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{product.attributes.name}}</p>
+</template>
+
+<script>
+export default {
+    name: 'Product',
+    props: {
+        product: Object,
+    },
+    components: {
+        
+    },
+    setup() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
