19afc21c2cae7a974d68941a93a941aa162b798b
hrdax <juanpa_200018@hotmail.com>
2023-08-23 17:52:33 -0400

    obtener productos en carro y anadir multiples

--- vue-ecommerce/client/src/api/cart.js
+++ vue-ecommerce/client/src/api/cart.js
@@ -1,8 +1,16 @@
 import { API_URL, PRODUCTS } from "../utils/constants"
 
 export function addProductCartApi(idProduct) {
-    const products = []
+    const products = getCartApi()
     products.push(idProduct)
 
     localStorage.setItem(PRODUCTS, JSON.stringify(products))
+}
+
+export function getCartApi() {
+    const products = localStorage.getItem(PRODUCTS)
+
+    if (!products) return []
+
+    return JSON.parse(products)
 }
