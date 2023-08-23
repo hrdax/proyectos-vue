4ce9a6e15acbd20b0fd317db346a6984be466079
hrdax <juanpa_200018@hotmail.com>
2023-08-23 16:34:36 -0400

    mostrando componente y productos en home

--- vue-ecommerce/client/src/views/HomeView.vue
+++ vue-ecommerce/client/src/views/HomeView.vue
@@ -1,25 +1,35 @@
 <template>
-  <div class="home">
-    <BasicLayout> <h1>Home</h1> </BasicLayout>
-  </div>
+  <BasicLayout>
+      <h1>Productos</h1>
+      <div class="ui grid">
+        <div 
+        class="sixten wide mobile eight wide tablet four wide computer column"
+        v-for="product in products"
+        :key="product.id"
+        >
+          <Product :product="product" />
+        </div>
+      </div>
+  </BasicLayout>
 </template>
 
 <script>
 import {ref, onMounted} from 'vue'
 import BasicLayout from '../layouts/BasicLayout'
 import { getProductsApi} from '../api/product'
+import Product from '../components/Product.vue'
 
 export default {
   name: 'Home',
   components: {
     BasicLayout,
-
+    Product,
   },
   setup() {
     let products = ref(null)
 
     onMounted (async () => {
-      const response = await getProductsApi(3)
+      const response = await getProductsApi()
       products.value = response.data
       console.log(response.data)
     })
