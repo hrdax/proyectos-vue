9bf0d338f5ba1eb9179bc1ad1489061c6e686762
hrdax <juanpa_200018@hotmail.com>
2023-08-23 18:00:19 -0400

    componente cart

---
+++ vue-ecommerce/client/src/components/Cart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cart">
+    <h2>Carrito de compras</h2>
+    <button>Cerrar Carrito</button>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'cart',
+}
+</script>
+
+<style lang="scss" scoped>
+.cart {
+    position: fixed;
+    right: 0;
+    top: 0;
+    width: 300px;
+    height: 100vh;
+    background-color: #fff;
+    box-shadow: 0px 0px 26px 5px rgba(0,0,0,0.75);
+    display: felx;
+    flex-direction: column;
+    justify-content: space-between;
+    transform: translateX(150%);
+
+    &.open {
+        transform: translateX(0);
+    }
+}
+</style>
