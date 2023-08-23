f9c9cd5f38706760735a82abfccac4335ff6400a
hrdax <juanpa_200018@hotmail.com>
2023-08-23 18:43:49 -0400

    obtener productos del carro

--- vue-ecommerce/client/src/api/cart.js
+++ vue-ecommerce/client/src/api/cart.js
@@ -1,5 +1,6 @@
 import { API_URL, PRODUCTS } from "../utils/constants"
 
+
 export function addProductCartApi(idProduct) {
     const products = getCartApi()
     products.push(idProduct)
@@ -13,4 +14,25 @@ export function getCartApi() {
     if (!products) return []
 
     return JSON.parse(products)
+}
+
+export async function getProductsCartApi(){
+    const idProducts = getCartApi()
+
+    if(idProducts.length === 0) return null
+
+    try {
+        const products = []
+
+        for await (const idProduct of idProducts) {
+            const response = await fetch(`${API_URL}/api/productos/${idProduct}`)
+            const result = await response.json()
+            products.push(result)
+        }
+
+        console.log(products)
+    } catch (error) {
+        console.log(error)
+        return null
+    }
 }
