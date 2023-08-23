b2c45c7b1ec9ab51a1756eb7f4f8f1ccb4585478
hrdax <juanpa_200018@hotmail.com>
2023-08-23 15:07:41 -0400

    componente category

---
+++ vue-ecommerce/client/src/views/Category.vue
@@ -0,0 +1,29 @@
+<template>
+  <BasicLayout>
+    <h1>Categoría de </h1>
+  </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from '../layouts/BasicLayout.vue'; 
+
+export default {
+  name: 'Category',
+  components: {
+    BasicLayout,
+  },
+  watch: {
+    $route(to, from) {
+      console.log(to, from);
+    }
+  },
+  setup() {
+  
+  }
+}
+
+</script>
+
+<style>
+
+</style>
