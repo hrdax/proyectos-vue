c4be61be94626b1db892d1f4a1bd2d7d4455419b
hrdax <juanpa_200018@hotmail.com>
2023-08-23 17:35:47 -0400

    visualizando productos por categoria

--- vue-ecommerce/client/src/views/Category.vue
+++ vue-ecommerce/client/src/views/Category.vue
@@ -1,7 +1,14 @@
 <template>
   <BasicLayout>
-    <h1>Categoría de </h1>
-    {{products}}
+    <div class="ui grid">
+        <div 
+        class="sixten wide mobile eight wide tablet four wide computer column"
+        v-for="product in products"
+        :key="product.id"
+        >
+            <Product :product="product" />
+        </div>
+    </div>
   </BasicLayout>
 </template>
 
@@ -10,11 +17,13 @@ import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import BasicLayout from '../layouts/BasicLayout.vue'; 
 import { getProductsCategory } from '../api/product'
+import Product from '../components/Product.vue'
 
 export default {
   name: 'Category',
   components: {
     BasicLayout,
+    Product,
   },
   watch: {
     $route(to, from) {
