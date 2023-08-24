ba214f77c47bbe39e46b834ed75d549b5f9e4d51
hrdax <juanpa_200018@hotmail.com>
2023-08-23 20:15:08 -0400

    componente cartbody

---
+++ vue-ecommerce/client/src/components/Cart/CartBody.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cart-body">
+    <h2>Productos</h2>
+    <p>{{ products }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'CartBody',
+ props: {
+    products: Array,
+ },
+ setup() {
+
+    return {
+    }
+ }
+
+}
+</script>
+
+<style>
+
+</style>
