a0a0e1d71d63e650e09cb3c547e0d25ebab3af38
hrdax <juanpa_200018@hotmail.com>
2023-08-23 20:38:00 -0400

    visualizando productos y estilos

--- vue-ecommerce/client/src/components/Cart/CartBody.vue
+++ vue-ecommerce/client/src/components/Cart/CartBody.vue
@@ -1,25 +1,91 @@
 <template>
-  <div class="cart-body">
-    <h2>Productos</h2>
-    <p>{{ products }}</p>
+  <div class="cart-body" v-if="products">
+    <div class="product" v-for="product in products" :key="product.data.id">
+        <img 
+        class="ui image fluid" 
+        :src="API_URL + product.data.attributes.image.data.attributes.url" 
+        :alt="product.data.attributes.name"
+        />
+        <div class="info">
+            <p>{{product.data.attributes.name}}</p>
+            <div class="price">
+                <p>${{product.data.attributes.price}}</p>
+                <div class="quantity">
+                    <button class="ui button primary" size="large">+</button>
+                    <p>{{product.quantity}}</p>
+                    <button class="ui button primary" size="large">-</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { API_URL } from '../../utils/constants'
+
 export default {
  name: 'CartBody',
  props: {
     products: Array,
  },
- setup() {
-
+ setup(props) {
+    
     return {
+        API_URL,
     }
  }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.cart-body {
+    padding: 20px 10px;
+    overflow-y: scroll;
+    height: calc(100vh - 50px - 90px);
+    &::-webkit-scrollbar {
+        display: none;
+    }
+
+    .product {
+        display: flex;
+
+        .ui.image.fluid {
+            width: 100px !important;
+
+        }
+    }
+
+    .info {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+    }
+
+    .price {
+        display: flex;
+        justify-content: space-between;
+        p {
+            margin: 0;
+        }
+    }
+
+    .quantity {
+        display: flex;
+        align-items: center;
+
+        p {
+            margin: 0 10px;
+        }
+
+        button {
+            margin: 4px 10px;
+        }
+    }
+
+}
 
 </style>
