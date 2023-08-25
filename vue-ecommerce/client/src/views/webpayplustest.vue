6df86ca102d83e0aa3789e5416353f0f9c986424
hrdax <juanpa_200018@hotmail.com>
2023-08-24 21:59:57 -0400

    probando webpay, intentar arreglar error CORS

--- vue-ecommerce/client/src/views/webpayplustest.vue
+++ vue-ecommerce/client/src/views/webpayplustest.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h1>Prueba de Webpay Plus</h1>
-    <form method="post" :action="response.url" v-if="response">
+    <form method="post" :action="response.url">
+        {{response.url}}
+        {{response.token}}
+
         <input type="hidden" name="token_ws" :value="response.token" />
         <input type="submit" value="Ir a pagar" />
     </form>
@@ -10,29 +13,29 @@
 
 <script>
 import { WebpayPlus, Options, IntegrationCommerceCodes, IntegrationApiKeys, Environment } from 'transbank-sdk';
-import { ref, onMounted } from 'vue';
 
 export default {
     name: 'WebpayPlusTest',
     setup() {
-        const buyOrder = 'ordenCompra12345678';
-        const sessionId = 'sesion1234557545';
+        const buyOrder = 'ordenCompra123';
+        const sessionId = 'sesion123';
         const amount = 10000;
         const returnUrl = 'http://localhost:8080/webpayplusresult';
-        console.log('respuesta ') 
         const tx = new WebpayPlus.Transaction(new Options(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, Environment.Integration));
 
-
-        onMounted(async () => {
-            await transaction()
-        })
         const transaction = async () => {
-            console.log('respuesta ')
-            const response = await tx.create(buyOrder, sessionId, amount, returnUrl);
-            return response
+            try {
+                const response = await tx.create(buyOrder, sessionId, amount, returnUrl);
+                return response
+
+            } catch (e) {
+                console.log("Error", e)
+                return null
+            }
         }
 
         return {
+            response: transaction()
         }
     }
 };
