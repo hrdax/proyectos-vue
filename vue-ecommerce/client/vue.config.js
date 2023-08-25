5fd563a1746d109d6cef4aef94c84e730da47fcc
hrdax <juanpa_200018@hotmail.com>
2023-08-25 10:04:06 -0400

    s

--- vue-ecommerce/client/vue.config.js
+++ vue-ecommerce/client/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'https://webpay3gint.transbank.cl',
+    proxy: 'http://localhost:1337',
   }
 })
