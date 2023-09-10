3d3423ba7d8afb8f3e55d55c170a1235e5f04b20
hrdax <juanpa_200018@hotmail.com>
2023-09-10 20:49:40 -0300

    verificar si se han cargado los productos

--- vue-ecommerce/client/src/views/HomeView.vue
+++ vue-ecommerce/client/src/views/HomeView.vue
@@ -9,30 +9,46 @@
         >
           <Product :product="product" />
         </div>
+        <div v-if="products === null " class="container">
+          <span>Encendiendo servidor Por Favor Espere<ClipLoader></ClipLoader></span>
+        </div>
       </div>
   </BasicLayout>
 </template>
 
 <script>
-import {ref, onMounted} from 'vue'
+import {ref, onMounted, watch} from 'vue'
 import BasicLayout from '../layouts/BasicLayout'
 import { getProductsApi} from '../api/product'
 import Product from '../components/Product.vue'
+import ClipLoader from '../components/ClipLoader.vue'
 
 export default {
   name: 'Home',
   components: {
     BasicLayout,
     Product,
+    ClipLoader
   },
   setup() {
     let products = ref(null)
 
     onMounted (async () => {
-      const response = await getProductsApi()
-      products.value = response.data
+      try {
+        let response = await getProductsApi();
+        products.value = response.data;
+        
+      } catch (e) {
+        const intervalData = setInterval(async () => {
+          let response = await getProductsApi();
+          products.value = response.data;
+          if (products.value != null){
+            clearInterval(intervalData);
+          }
+        }, 15000);
+      }
     })
-
+    
     return {
       products,
     }
